1461b9591c1687a4b8ca5cee2ba95180b36c3699
makennamartin97 <makennamartin97@yahoo.com>
2021-07-07 19:40:33 -0400

    pushfront

--- chapter2-arrays/basicarray.js
+++ chapter2-arrays/basicarray.js
@@ -30,3 +30,20 @@ function popfront(arr){
 }
 console.log(popfront([0,1,2,3,4])) // [ 1, 2, 3, 4 ], 0
 console.log(popfront([6,5,4,3])) // [ 5, 4, 3 ], 6
+
+// pushfront
+// Given array and an additional value, insert this value at the beginning
+// of the array. Do this without using any built-in array methods.
+
+function pushfront(arr, val){
+    // rather than .push..
+    arr[arr.length] = val
+    // for loop starting at the end to swap to the front
+    for(let i = arr.length-1; i>0; i--){
+        let front = arr[i]
+        arr[i] = arr[i-1]
+        arr[i-1] = front
+    }
+    return arr
+}
+console.log(pushfront([1,2,3,4,5],0)) // [ 0, 1, 2, 3, 4, 5 ]
