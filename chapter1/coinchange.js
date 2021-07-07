f8a8a2edb8827ee4d50220f4dffd9386f1e5bc32
makennamartin97 <makennamartin97@yahoo.com>
2021-07-07 19:15:58 -0400

    coin change algo

--- chapter1/coinchange.js
+++ chapter1/coinchange.js
@@ -3,20 +3,19 @@
 // number of cents, and computes how to represent that amount with the
 // smallest number of coins. Console.log the result.
 
-function generateCoinChange(cents)​{
-    let coins = [25, 10, 5, 1]
-    let result = []
-    let i = 0;
-    while(cents > 0 && cents - coins[i] > 0){
-        if(cents - coins[i] >= 0){
-            cents = cents - coins[i]
-            result.push(coins[i])
-            continue
-        }
-        i++
-        
-    }
-    console.log(result)
+function coinchange(cents){
+    // starting with a quarter, let's see how many quarters can fit into the amt given
+    console.log("Quarters:" + Math.trunc(cents/25));
+    // now we update the amount to the REMAINDER, bc we want to know how much is left rather how many times it goes into 
+    cents = cents%25;
+    console.log("Dimes:"+ Math.trunc(cents/10));
+    cents = cents%10;
+    console.log("Nickels:"+Math.trunc(cents/5));
+    cents = cents%5
+    console.log("Pennies:"+ Math.trunc(cents/1));
+    cents = cents%1
+    return cents
 
 }
-console.log(generateCoinChange(84))
+var cents = 84
+console.log(coinchange(cents))
